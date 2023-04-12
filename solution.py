160c74628602669863941769bae1fad92d8f06a9
squrem <108416846+squrem@users.noreply.github.com>
2023-04-12 20:49:22 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-
+from hyppo.ksample import MMD
 
 chat_id = 434559054 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  if MMD(compute_kernel="rbf", gamma=1).test(x, y)[1] < 0.01:
+    return True
+  else:
+    return False 
